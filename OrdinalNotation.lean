e86228ab0d052a8fcdd9594c97b5eeccc77ac17b
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-30 14:30:12 -0600

    rename

--- OrdinalNotation.lean
+++ OrdinalNotation.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `OrdinalNotation` library.
 -- Import modules here that should be built as part of the library.
-import OrdinalNotation.Basic
+import OrdinalNotation.Cantor
